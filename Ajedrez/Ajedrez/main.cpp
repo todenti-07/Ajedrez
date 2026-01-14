24beb4f3edd85442a42ce2ca38de1e5ded80e4a8
Laia Saavedra Cusco <laia.saavedra@estudiant.enti.cat>
2026-01-14 09:20:14 +0100

    Movimientos Piezas

--- Ajedrez/Ajedrez/main.cpp
+++ Ajedrez/Ajedrez/main.cpp
@@ -1,102 +1,62 @@
-#include <iostream>
+#include "const.h"
+#include "tableroRelacion.h"
 
-#define TABLERO 8
-#define VACIçO '*'
-
-#define PE”N_BLAN 'P'
-#define TORRE_BLAN 'T'
-#define CABALLO_BLAN 'H'
-#define ALFIL_BLAN 'B'
-#define REINA_BLAN 'Q'
-#define REY_BLAN 'K'
-
-#define PE”N_NEGR 'p'
-#define TORRE_NEGR 't'
-#define CABALLO_NEGR 'h'
-#define ALFIL_NEGR 'b'
-#define REINA_NEGR 'q'
-#define REY_NEGR 'k'
-
-void inicializarTablero(char tablero[TABLERO][TABLERO]) {
-	for (int i = 0; i < TABLERO; i++) {
-		for (int j = 0; j < TABLERO; j++) {
-			tablero[i][j] = VACIO;         
-			if (i == 1) {
-				tablero[i][j] = 'p'; // Peones Negros
-			}
-			else if (i == 6) {
-				tablero[i][j] = 'P'; // Peones Blancos
-			}
-		}
+void comprobarMovimientoCualquiera(char tablero[TABLERO][TABLERO], Coordenadas posInicial, Coordenadas posFinal) {
+	// Ajuste de Ìndices para el tablero.
+	while (posFinal.x <= 8 || posFinal.x > 0) {
+		std::cout << "Ese numero no se encuentra en el tablero!";
+		std::cin >> posFinal.x;
 	}
-	
-	for (int i = 0; i < 8; i++) {
-
-		// Fila 0: piezas negras
-		if (i == 0 || i == 7) tablero[0][i] = TORRE_NEGR;        // 't'
-		else if (i == 1 || i == 6) tablero[0][i] = CABALLO_NEGR; // 'h'
-		else if (i == 2 || i == 5) tablero[0][i] = ALFIL_NEGR;   // 'b'
-		else if (i == 3) tablero[0][i] = REINA_NEGR;               // 'q'
-		else if (i == 4) tablero[0][i] = REY_NEGR;                 // 'k'
-
-		// Fila 7: piezas blancas
-		if (i == 0 || i == 7) tablero[7][i] = TORRE_BLAN;        // 'T'
-		else if (i == 1 || i == 6) tablero[7][i] = CABALLO_BLAN; // 'H'
-		else if (i == 2 || i == 5) tablero[7][i] = ALFIL_BLAN;   // 'B'
-		else if (i == 3) tablero[7][i] = REINA_BLAN;               // 'Q'
-		else if (i == 4) tablero[7][i] = REY_BLAN;                 // 'K'
+	while (posFinal.y <= 8 || posFinal.y > 0) {
+		std::cout << "Ese numero no se encuentra en el tablero!";
+		std::cin >> posFinal.y;
 	}
-
-}
-
-void mostrarTablero(char tablero[TABLERO][TABLERO]) {
-	std::cout << "  ";
-	for (int i = 1; i < TABLERO + 1; i++) {
-		std::cout << i << " ";
+	// Se tiene que limitar al jugador para que no pueda mover piezas del oponente.
+	while (tablero[posInicial.x][posInicial.y] < 'A' || tablero[posInicial.x][posInicial.y] > 'Z') {
+		std::cout << "No puedes mover las piezas del oponente! Elige otra pieza: (columna, fila)";
+		std::cin >> posInicial.x;
+		std::cin >> posInicial.y;
 	}
-	std::cout << std::endl;
-	for (int i = TABLERO; i > 0; i--) {
-		std::cout << i << " ";
-
-		for (int j = 0; j < TABLERO; j++) {
-			std::cout << tablero[i - 1][j] << " ";
-		}
-		std::cout << std::endl;
+	// Se tiene que limitar al jugador para que pueda mover las piezas a las posiciones posibles.
+	while (tablero[posFinal.x][8 - posFinal.y * -1] != VACIO) {
+		std::cout << "No puedes mover la pieza a esa posicion! Elige otra posicion: (columna, fila)";
+		std::cin >> posFinal.x;
+		std::cin >> posFinal.y;
 	}
 }
+
 
 void juegoJugable(char tablero[TABLERO][TABLERO]) {
-	int respuestaPiezaCInicial, respuestaPiezaFInicial;
-	int respuestaPiezaCFinal, respuestaPiezaFFinal;
+
+	Coordenadas posInicial, posFinal;
 	bool juegoAcabado = false;
 	bool esTurnoJ1 = true;
 
-	std::cout << "\nInicia el jugador de piezas blancas. Indica la posiciÛn de la pieza que quieras mover: (columna, fila)"<<std::endl; 
+	std::cout << "\nInicia el jugador de piezas blancas. Indica la posicion de la pieza que quieras mover: (columna, fila)" << std::endl;
 	while (!juegoAcabado) {
 		while (esTurnoJ1) { //Jugador 1
-			std::cin >> respuestaPiezaCInicial;
-			std::cin >> respuestaPiezaFInicial;
-			std::cout << "Indica la posiciÛn a la que quieres que se mueva la pieza: (columna, fila)" ;
-			std::cin >> respuestaPiezaCFinal;
-			std::cin >> respuestaPiezaFFinal;
-			tablero[respuestaPiezaCFinal][respuestaPiezaFFinal] = tablero[respuestaPiezaCInicial][respuestaPiezaFInicial];
-			tablero[respuestaPiezaCInicial][respuestaPiezaFInicial] = VACÕO;
+			std::cin >> posInicial.x;
+			std::cin >> posInicial.y;
+			std::cout << "Indica la posicion a la que quieres que se mueva la pieza: (columna, fila)";
+			std::cin >> posFinal.x;
+			std::cin >> posFinal.y;
+			comprobarMovimientoCualquiera(tablero, posInicial, posFinal);
+			tablero[posFinal.x][8 - posFinal.y * -1] = tablero[posInicial.x][posInicial.y];
+			tablero[posInicial.x][posInicial.y] = VACIO;
 			esTurnoJ1 = false;
 		}
 		while (!esTurnoJ1) { //Jugador 2
-			std::cin >> respuestaPiezaCInicial;
-			std::cin >> respuestaPiezaFInicial;
-			std::cout << "Indica la posiciÛn a la que quieres que se mueva la pieza: (columna, fila)";
-			std::cin >> respuestaPiezaCFinal;
-			std::cin >> respuestaPiezaFFinal;
-			tablero[8 - respuestaPiezaCFinal * -1][8 - respuestaPiezaFFinal * -1] = tablero[respuestaPiezaCInicial][respuestaPiezaFInicial];
-			tablero[respuestaPiezaCInicial][respuestaPiezaFInicial] = VACÕO;
+			std::cin >> posInicial.x;
+			std::cin >> posInicial.y;
+			std::cout << "Indica la posicion a la que quieres que se mueva la pieza: (columna, fila)";
+			std::cin >> posFinal.x;
+			std::cin >> posFinal.y;
+			comprobarMovimientoCualquiera(tablero, posInicial,posFinal);
+			tablero[posFinal.x][8 - posFinal.y * -1] = tablero[posInicial.x][posInicial.y];
+			tablero[posInicial.x][posInicial.y] = VACIO;
 			esTurnoJ1 = true;
 		}
 	}
-	
-
-
 }
 
 int main() {
@@ -106,7 +66,7 @@ int main() {
 
 	mostrarTablero(tablero);
 
-	juegoJugable(tablero); 
+	juegoJugable(tablero);
 
 	return 0;
 }
---
+++ Ajedrez/Ajedrez/movimientosVerificados.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <cstdlib>
+#include "const.h"
+#include "tableroRelacion.h"
+
+void verificarMovimientoPeonB(char tablero[TABLERO][TABLERO], Coordenadas posInicial, Coordenadas posFinal) {
+	
+	if (posInicial.y == 7 && posInicial.y - 2 == posFinal.y || posInicial.y - 1 == posFinal.y) {
+		std::cout << "Es posible el movimiento";
+	}
+	else if (posInicial.x - 1 != '~' && posFinal.y - 1 || posInicial.x + 1 != '~' && posFinal.y - 1) { // Captura diagonal
+		std::cout << "Es posible el movimiento";
+	}
+	else {
+		std::cout << "Movimiento no permitido para el peon blanco. Introduce de nuevo las coordenadas finales: \n";
+		std::cin >> posFinal.x;
+		std::cin >> posFinal.y;
+		verificarMovimientoPeonB(tablero, posInicial, posFinal);
+	}
+
+}
+
+void verificarMovimientoPeonN(char tablero[TABLERO][TABLERO], Coordenadas posInicial, Coordenadas posFinal) {
+	if (posInicial.y == 2 && posInicial.y + 2 == posFinal.y || posInicial.y + 1 == posFinal.y) {
+		std::cout << "Es posible el movimiento";
+	}
+	else if (posInicial.x - 1 != '~' && posFinal.y + 1 || posInicial.x + 1 != '~' && posFinal.y + 1) { // Captura diagonal
+		std::cout << "Es posible el movimiento";
+	}
+	else {
+		std::cout << "Movimiento no permitido para el peon negro. Introduce de nuevo las coordenadas finales: \n";
+		std::cin >> posFinal.x;
+		std::cin >> posFinal.y;
+		verificarMovimientoPeonN(tablero, posInicial, posFinal);
+	}
+}
+
+void verificarMovimientoAlfilB(char tablero[TABLERO][TABLERO], Coordenadas posInicial, Coordenadas posFinal) {
+	
+	for (int i = 0; i < TABLERO; i++) { // Horizontal
+		for (int j = 0; j < TABLERO; j++) { // Vertical
+			if (tablero[posInicial.x + j][posInicial.y + j] != '~' || tablero[posInicial.x - j][posInicial.y + j] != '~' || tablero[posInicial.x + j][posInicial.y - j] != '~' || tablero[posInicial.x - j][posInicial.y - j] != '~'){
+				while (tablero[posInicial.x][posInicial.y] < 'A' || tablero[posInicial.x][posInicial.y] > 'Z') {
+					std::cout << "No puedes eliminar tus propias piezas! Elige otro movimiento: (columna, fila)";
+					std::cin >> posFinal.x;
+					std::cin >> posFinal.y;
+				}
+			}
+		}
+	}
+}
+
+void verificarMovimientoAlfilN(char tablero[TABLERO][TABLERO], Coordenadas posInicial, Coordenadas posFinal) {
+	
+	for (int i = 0; i < TABLERO; i++) { // Horizontal
+		for (int j = 0; j < TABLERO; j++) { // Vertical
+			if (tablero[posInicial.x + j][posInicial.y + j] != '~' || tablero[posInicial.x - j][posInicial.y + j] != '~' || tablero[posInicial.x + j][posInicial.y - j] != '~' || tablero[posInicial.x - j][posInicial.y - j] != '~') {
+				while (tablero[posInicial.x][posInicial.y] < 'a' || tablero[posInicial.x][posInicial.y] > 'z') {
+					std::cout << "No puedes eliminar tus propias piezas! Elige otro movimiento: (columna, fila)";
+					std::cin >> posFinal.x;
+					std::cin >> posFinal.y;
+				}
+			}
+		}
+	}
+}
+
+void verificarMovimientoTorreB(char tablero[TABLERO][TABLERO], Coordenadas posInicial, Coordenadas posFinal) {
+	
+	if (posInicial.x != posFinal.x && posInicial.y != posFinal.y) {
+		std::cout << "La torre solo se mueve en horitzontal o vertical.\n";
+		return;
+	}
+
+	if (tablero[posFinal.x][posFinal.y] >= 'A' && tablero[posFinal.x][posFinal.y] <= 'Z') {
+		std::cout << "No puedes eliminar tus propias piezas.\n";
+		return; 
+	}
+
+	std::cout << "Movimiento valido!\n"; 
+}
+
+void verificarMovimientoTorreN(char tablero[TABLERO][TABLERO], Coordenadas posInicial, Coordenadas posFinal) {
+	
+	if (posInicial.x != posFinal.x && posInicial.y != posFinal.y) {
+		std::cout << "La torre solo se mueve en horizontal o vertical.\n";
+		return;
+	}
+
+	if (tablero[posFinal.x][posFinal.y] >= 'a' && tablero[posFinal.x][posFinal.y] <= 'z') {
+		std::cout << "No puedes eliminar tus propias piezas.\n";
+		return;
+	}
+
+	std::cout << "Movimiento valido!\n";
+}
+
+//FALTA CABALLO
+
+void verificarMovimientoReinaB(char tablero[TABLERO][TABLERO], Coordenadas posInicial, Coordenadas posFinal) {
+
+	if (posInicial.x == posFinal.x || posInicial.y == posFinal.y ||
+		posFinal.x - posInicial.x == posFinal.y - posInicial.y || 
+		posFinal.x - posInicial.x == -(posFinal.y - posInicial.y)) { 
+
+		if (tablero[posFinal.x][posFinal.y] >= 'A' && tablero[posFinal.x][posFinal.y] <= 'Z') {
+			std::cout << "No puedes eliminar tus propias piezas.\n";
+		}
+		else {
+			std::cout << "Movimiento valido\n";
+		}
+
+	}
+	else {
+		std::cout << "Movimiento invalido de la reina.\n";
+	}
+}
+
+void verificarMovimientoReinaB(char tablero[TABLERO][TABLERO], Coordenadas posInicial, Coordenadas posFinal) {
+
+	if (posInicial.x == posFinal.x || posInicial.y == posFinal.y ||
+		posFinal.x - posInicial.x == posFinal.y - posInicial.y ||
+		posFinal.x - posInicial.x == -(posFinal.y - posInicial.y)) {
+
+		if (tablero[posFinal.x][posFinal.y] >= 'a' && tablero[posFinal.x][posFinal.y] <= 'z') {
+			std::cout << "No puedes eliminar tus propias piezas.\n";
+		}
+		else {
+			std::cout << "Movimiento valido\n";
+		}
+
+	}
+	else {
+		std::cout << "Movimiento invalido de la reina.\n";
+	}
+}
+
+//FALTA REY
